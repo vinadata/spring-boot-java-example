3d83d4fe20e6701e2461f13eb6130a94558f6ddd
Thai Nguyen <thainn2@vng.com.vn>
2019-08-08 10:09:20 +0700

    init spring-boot-java-example

---
+++ src/main/java/com/vindata/ci/example/springboot/SpringBootJavaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.vindata.ci.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJavaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vindata/ci/example/springboot/SpringBootJavaExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vindata.ci.example.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJavaExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
